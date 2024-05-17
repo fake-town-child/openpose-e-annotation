8573531dac958fb85fc44ed720849e2aea1a533f
Happa8 <happa.eight@gmail.com>
2024-05-17 11:13:21 +0900

    Enable react

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,10 @@
  * ```
  */
 
-import './index.css';
+import "./index.css";
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+import "./app";
+
+console.log(
+  '👋 This message is being logged by "renderer.js", included via webpack'
+);
