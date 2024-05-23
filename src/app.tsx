cf08033cda36f7182a5dc371490582650ea1e98e
Happa8 <happa.eight@gmail.com>
2024-05-23 09:52:12 +0900

    chore: Update dependencies and configuration files

--- src/app.tsx
+++ src/app.tsx
@@ -15,6 +15,7 @@ import {
   layerListAtomsAtom,
 } from "./stores/atom";
 import { Layer } from "@/types";
+import { Box, Button, ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 const App: FC = () => {
   const [currentCanvasSize, setCanvasSize] = useAtom(canvasSizeAtom);
@@ -25,146 +26,154 @@ const App: FC = () => {
   const appState = useAtomValue(appSateAtom);
 
   return (
-    <div className="app-container">
-      <aside>
-        {/* sidebar */}
-        <button
-          className="button"
-          onClick={() => {
-            setIsSaveImageMode(true);
-            const dataURL = appState.layerList[0].ref?.toDataURL({
-              pixelRatio: appState.layerList[0].ref.parent
-                ? 1 / appState.layerList[0].ref.parent?.scaleX()
-                : 1,
-            });
-            if (dataURL) {
+    <ChakraProvider>
+      <Box className="app-container" bgColor={"gray.50"}>
+        <Box
+          as="aside"
+          maxWidth={400}
+          borderColor={"gray.200"}
+          bgColor={"white"}
+          borderWidth={1}
+          borderRadius={""}
+          p={4}
+          flexGrow={1}
+        >
+          {/* sidebar */}
+          <Button
+            // className="button"
+            onClick={() => {
+              setIsSaveImageMode(true);
+              const dataURL = appState.layerList[0].ref?.toDataURL({
+                pixelRatio: appState.layerList[0].ref.parent
+                  ? 1 / appState.layerList[0].ref.parent?.scaleX()
+                  : 1,
+              });
+              if (dataURL) {
+                window.electronAPI
+                  .getFileNameWithSaveDialog({
+                    payload: {
+                      defaultPath: "save.png",
+                      filters: [{ name: "PNG", extensions: ["png"] }],
+                    },
+                  })
+                  .then((filePath) => {
+                    window.electronAPI
+                      .saveFile({
+                        filePath: filePath,
+                        data: PNGdataURLtoBuffer(dataURL),
+                      })
+                      .then((result) => {
+                        console.log("save success");
+                      });
+                  })
+                  .catch((err) => {
+                    console.error(err);
+                  });
+              }
+              setIsSaveImageMode(false);
+            }}
+          >
+            save image
+          </Button>
+          <Button
+            className="button"
+            onClick={() => {
+              window.electronAPI
+                .getFileNamesWithOpenDialog({
+                  payload: {
+                    filters: [
+                      { name: "Image", extensions: ["png", "jpg", "jpeg"] },
+                    ],
+                    properties: ["openFile"],
+                  },
+                })
+                .then((filePaths) => {
+                  const filePath = filePaths[0];
+                  window.electronAPI
+                    .getFile({ filePath: filePath })
+                    .then((data) => {
+                      const newLayer: Layer = {
+                        name: "bgImage",
+                        type: "image",
+                        src: BufferToPNGDataURL(data),
+                        ref: null,
+                      };
+                      dispatchListAtoms({
+                        type: "insert",
+                        value: newLayer,
+                        before: layerListAtoms[0],
+                      });
+                    });
+                })
+                .catch((err) => {
+                  console.error(err);
+                });
+            }}
+          >
+            open image
+          </Button>
+          <Button
+            onClick={() => {
+              window.electronAPI
+                .getFileNamesWithOpenDialog({
+                  payload: {
+                    filters: [{ name: "JSON", extensions: ["json"] }],
+                    properties: ["openFile"],
+                  },
+                })
+                .then((filePaths) => {
+                  const filePath = filePaths[0];
+                  window.electronAPI
+                    .getFile({ filePath: filePath })
+                    .then((data) => {
+                      const { layerList, size } = LoadSaveFile(data);
+                      setLayerList(layerList);
+                      if (size) {
+                        setCanvasSize(size);
+                      }
+                    });
+                });
+            }}
+          >
+            open json
+          </Button>
+          <Button
+            onClick={() => {
+              setCanvasSize({ width: 1024, height: 512 });
+            }}
+          >
+            change size
+          </Button>
+          <Button
+            onClick={() => {
+              const saveFile = GenerateSaveFile(appState);
               window.electronAPI
                 .getFileNameWithSaveDialog({
                   payload: {
-                    defaultPath: "save.png",
-                    filters: [{ name: "PNG", extensions: ["png"] }],
+                    defaultPath: "save.json",
+                    filters: [{ name: "JSON", extensions: ["json"] }],
                   },
                 })
                 .then((filePath) => {
                   window.electronAPI
                     .saveFile({
                       filePath: filePath,
-                      data: PNGdataURLtoBuffer(dataURL),
+                      data: saveFile,
                     })
                     .then((result) => {
                       console.log("save success");
                     });
-                })
-                .catch((err) => {
-                  console.error(err);
                 });
-            }
-            setIsSaveImageMode(false);
-          }}
-        >
-          save image
-        </button>
-        <button
-          className="button"
-          onClick={() => {
-            window.electronAPI
-              .getFileNamesWithOpenDialog({
-                payload: {
-                  filters: [
-                    { name: "Image", extensions: ["png", "jpg", "jpeg"] },
-                  ],
-                  properties: ["openFile"],
-                },
-              })
-              .then((filePaths) => {
-                const filePath = filePaths[0];
-                window.electronAPI
-                  .getFile({ filePath: filePath })
-                  .then((data) => {
-                    const newLayer: Layer = {
-                      name: "bgImage",
-                      type: "image",
-                      src: BufferToPNGDataURL(data),
-                      ref: null,
-                    };
-                    dispatchListAtoms({
-                      type: "insert",
-                      value: newLayer,
-                      before: layerListAtoms[0],
-                    });
-                  });
-              })
-              .catch((err) => {
-                console.error(err);
-              });
-          }}
-        >
-          open image
-        </button>
-        <button
-          className="button"
-          onClick={() => {
-            window.electronAPI
-              .getFileNamesWithOpenDialog({
-                payload: {
-                  filters: [{ name: "JSON", extensions: ["json"] }],
-                  properties: ["openFile"],
-                },
-              })
-              .then((filePaths) => {
-                const filePath = filePaths[0];
-                window.electronAPI
-                  .getFile({ filePath: filePath })
-                  .then((data) => {
-                    const { layerList, size } = LoadSaveFile(data);
-                    setLayerList(layerList);
-                    if (size) {
-                      setCanvasSize(size);
-                    }
-                  });
-              });
-          }}
-        >
-          open json
-        </button>
-        <button
-          className="button"
-          onClick={() => {
-            setCanvasSize({ width: 1024, height: 512 });
-          }}
-        >
-          change size
-        </button>
-        <button
-          className="button"
-          onClick={() => {
-            const saveFile = GenerateSaveFile(appState);
-            window.electronAPI
-              .getFileNameWithSaveDialog({
-                payload: {
-                  defaultPath: "save.json",
-                  filters: [{ name: "JSON", extensions: ["json"] }],
-                },
-              })
-              .then((filePath) => {
-                window.electronAPI
-                  .saveFile({
-                    filePath: filePath,
-                    data: saveFile,
-                  })
-                  .then((result) => {
-                    console.log("save success");
-                  });
-              });
-          }}
-        >
-          save json
-        </button>
-      </aside>
-      <main id="stage-parent">
-        <MainCanvas />
-      </main>
-    </div>
+            }}
+          >
+            save json
+          </Button>
+        </Box>
+        <main id="stage-parent">
+          <MainCanvas />
+        </main>
+      </Box>
+    </ChakraProvider>
   );
 };
 
